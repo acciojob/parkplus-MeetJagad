7203d36228954b13b2a7cb4b70d6e9ddf234ac0e
Meet Jagad <jagadmeet24@gmail.com>
2023-03-26 13:40:30 +0530

    ParkPlus

--- src/main/java/com/driver/repository/UserRepository.java
+++ src/main/java/com/driver/repository/UserRepository.java
@@ -1,5 +1,5 @@
 package com.driver.repository;
-import org.apache.catalina.User;
+import com.driver.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
